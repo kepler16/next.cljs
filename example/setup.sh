87602a35bb5b24a8d9d0e181ff368a64c09840c8
Alexis Vincent <mail@alexisvincent.io>
2021-09-11 22:58:42 +0100

    Add build stuff

---
+++ example/setup.sh
@@ -0,0 +1,4 @@
+amazon-linux-extras install java-openjdk11
+curl -O https://download.clojure.org/install/linux-install-1.10.1.763.sh
+chmod +x linux-install-1.10.1.763.sh
+./linux-install-1.10.1.763.sh
